69dacb662480d7a31faffeebce802e1d55980e72
VoidUpdate <matthew.w.shaw74@gmail.com>
2019-11-27 16:19:07 +0000

    I'm an idiot. Dont use OO when you dont need to

---
+++ Assets/Scripts/TestingActionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TestingActionManager : MonoBehaviour
+{
+
+    public Light LeftLight;
+    public Light RightLight;
+    public void LightSwitchL()
+    {
+        RightLight.enabled = true;
+    }
+
+    public void LightSwitchR()
+    {
+        LeftLight.enabled = true;
+    }
+}
