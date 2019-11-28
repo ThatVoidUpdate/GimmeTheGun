c1aee831447d82b432cb958728b21ce44b89d921
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-11-28 10:50:48 +0000

    Added axes for past and future player, Added player movement for past and future

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType{Past, Future}
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerType Player;
+    public float HorizontalSpeed;
+    public float JumpForce;
+
+    private Rigidbody2D rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        switch (Player)
+        {
+            case PlayerType.Past:
+                rb.velocity = new Vector2(Input.GetAxis("PastHorizontal") * HorizontalSpeed * Time.deltaTime, rb.velocity.y);
+                break;
+            case PlayerType.Future:
+                rb.velocity = new Vector2(Input.GetAxis("FutureHorizontal") * HorizontalSpeed * Time.deltaTime, rb.velocity.y);
+                break;
+        }
+    }
+}
