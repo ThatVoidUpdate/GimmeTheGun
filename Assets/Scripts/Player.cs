ce1e0acdd47d28dae71db91c92f8cef5def483ed
VoidUpdate <matthew.w.shaw74@gmail.com>
2019-11-28 14:50:56 +0000

    Added player movement, gun holding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+    [Header("Control Options")]
     public bool KeyboardPlayer;
     public int ControllerNumber;
 
+    [Header("Speeds")]
     public float HorizontalSpeed;
     public float VerticalSpeed;
+
+    [Header("Held object options")]
+    public GameObject HeldObject;
+    public Vector2 HeldOffset;
+    private List<Collider2D> colliders = new List<Collider2D>();
 
     private Rigidbody2D rb;
 
@@ -25,6 +32,21 @@ public class Player : MonoBehaviour
         if (KeyboardPlayer)
         {
             rb.velocity = new Vector2(Input.GetAxis("KeyboardHorizontal") * HorizontalSpeed, Input.GetAxis("KeyboardVertical") * VerticalSpeed);
+
+            if (Input.GetButton("KeyboardGrab") && colliders.Count > 0)
+            {
+                colliders[0].transform.position = gameObject.transform.position + new Vector3(HeldOffset.x, HeldOffset.y);
+            }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!colliders.Contains(other)) { colliders.Add(other); }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        colliders.Remove(other);
     }
 }
