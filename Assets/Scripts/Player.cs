f66a1597be9d749c98d9663d37168c550a04620f
VoidUpdate <matthew.w.shaw74@gmail.com>
2019-11-28 13:52:21 +0000

    Added background and player graphics, changed input settings for joysticks, added basic playe controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public bool KeyboardPlayer;
+    public int ControllerNumber;
+
+    public float HorizontalSpeed;
+    public float VerticalSpeed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (KeyboardPlayer)
+        {
+            rb.velocity = new Vector2(Input.GetAxis("KeyboardHorizontal") * HorizontalSpeed, Input.GetAxis("KeyboardVertical") * VerticalSpeed);
+        }
+    }
+}
