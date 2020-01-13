f0b70cfec95dc6f80d638c7ecd9d23561f282d1e
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-01-13 09:29:35 +0000

    Added PlayerSwitcher.cs, allows a debug player to switch between the player they are controlling

---
+++ Assets/Scripts/PlayerSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitcher : MonoBehaviour
+{
+    public Controller controller;
+    public Control NextPlayer;
+    public Control PrevPlayer;
+    public Player[] AllPlayers;
+    public int CurrentPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.GetControllerDown(NextPlayer))
+        {
+            AllPlayers[CurrentPlayer].controller = null;
+            CurrentPlayer++;
+            CurrentPlayer = CurrentPlayer % AllPlayers.Length;
+            AllPlayers[CurrentPlayer].controller = controller;
+        }
+        else if (controller.GetControllerDown(PrevPlayer))
+        {
+            AllPlayers[CurrentPlayer].controller = null;
+            CurrentPlayer--;
+            CurrentPlayer = CurrentPlayer == -1 ? AllPlayers.Length - 1 : CurrentPlayer;
+            AllPlayers[CurrentPlayer].controller = controller;
+        }
+    }
+}
