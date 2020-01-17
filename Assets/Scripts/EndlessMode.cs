b7fa0677ed6fd20f9187d033c7bbedc9e9c36931
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-01-17 11:36:23 +0000

    Fixed bug with endless mode where it skipped a lot of waves

--- Assets/Scripts/EndlessMode.cs
+++ Assets/Scripts/EndlessMode.cs
@@ -6,7 +6,7 @@ using System;
 
 public class EndlessMode : MonoBehaviour
 {
-    public int Level = 1;
+    public int Level = 0;
     public Spawner[] SpawnPoints;
 
     [Header("Enemies")]
@@ -18,29 +18,31 @@ public class EndlessMode : MonoBehaviour
 
     public List<GameObject> CurrentEnemies = new List<GameObject>();
 
-    public void Awake()
-    {//Spawn an initial wave
-        //calculate the amount of enemies we need to spawn, and spawn them
-        int EnemyCount = Mathf.CeilToInt(Level * Level / 2);
-        int FastEnemyCount = Mathf.CeilToInt((Level * Level / 30) - 1) > 0 ? Mathf.CeilToInt((Level * Level / 30) - 1) : 0;
+    private bool HandlingWave = false;
 
-        foreach (Spawner spawner in SpawnPoints)
-        {
-            CurrentEnemies.AddRange(spawner.Spawn(Enemy, EnemyCount));
-            CurrentEnemies.AddRange(spawner.Spawn(FastEnemy, FastEnemyCount));
-        }
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (CurrentEnemies.Count == 0)
-        {//We killed all the enemies
-            StartCoroutine(NewWave());            
-        }
-
         //remove any destroyed enemies
         CurrentEnemies = CurrentEnemies.Where(x => x != null).ToList();
+
+        if (Level == 0)
+        {//Spawn an initial wave (hardcoded for reason of bugs)
+            HandlingWave = true;
+
+            foreach (Spawner spawner in SpawnPoints)
+            {
+                CurrentEnemies.AddRange(spawner.Spawn(Enemy, 1));
+            }
+            HandlingWave = false;
+
+            Level++;
+        }
+
+        if (CurrentEnemies.Count == 0 && !HandlingWave)
+        {//We killed all the enemies
+            HandlingWave = true;
+            StartCoroutine(NewWave());
+        }
     }
 
     IEnumerator NewWave()
@@ -64,13 +66,14 @@ public class EndlessMode : MonoBehaviour
         }
 
         //calculate the amount of enemies we need to spawn, and spawn them
-        int EnemyCount = Mathf.CeilToInt(Level * Level / 2);
+        int EnemyCount = Mathf.CeilToInt(Level * Level / 2f);
         int FastEnemyCount = Mathf.CeilToInt((Level * Level / 30) - 1) > 0 ? Mathf.CeilToInt((Level * Level / 30) - 1) : 0;
 
         foreach (Spawner spawner in SpawnPoints)
         {
             CurrentEnemies.AddRange(spawner.Spawn(Enemy, EnemyCount));
             CurrentEnemies.AddRange(spawner.Spawn(FastEnemy, FastEnemyCount));
-        }     
+        }
+        HandlingWave = false;
     }
 }
