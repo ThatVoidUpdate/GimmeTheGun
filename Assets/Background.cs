b1899a128bf71c92152d8893f286ae59837b632f
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-01-19 13:14:08 +0000

    Added lava background. Added selection to background object

---
+++ Assets/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Backgrounds {Rocky, Lava}
+public class Background : MonoBehaviour
+{
+    public Backgrounds background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (background)
+        {
+            case Backgrounds.Rocky:
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Rocky");
+                break;
+            case Backgrounds.Lava:
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Lava");
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
