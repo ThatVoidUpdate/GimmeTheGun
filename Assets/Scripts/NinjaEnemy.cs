1fe7460355da5fa8bda3971081a430605ba27f15
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-01-19 16:25:22 +0000

    Added ninja triangle, with shurikens (not final graphics)

---
+++ Assets/Scripts/NinjaEnemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaEnemy : Enemy
+{
+    public float TeleportTime;
+    public float ThrowTime;
+    private bool HasThrown = false;
+    private float CurrentTime;
+
+    [Header("Teleport Zone")]
+    public Vector2 TopLeftTeleportZone;
+    public Vector2 BottomRightTeleportZone;
+
+    [Header("Shurikens")]
+    public GameObject Shuriken;
+    public float ShurikenSpeed;
+    public float ShurikenSpawnDistance;
+
+   public void Update()
+    {
+        CurrentTime += Time.deltaTime;
+
+        if (CurrentTime > TeleportTime)
+        {
+            Teleport();
+            CurrentTime = 0;
+            HasThrown = false;
+        }
+
+        if (CurrentTime > ThrowTime && !HasThrown)
+        {
+            ThrowShuriken();
+            HasThrown = true;
+        }
+    }
+
+    public void Teleport()
+    {
+        float TargetX = Random.Range(TopLeftTeleportZone.x, BottomRightTeleportZone.x);
+        float TargetY = Random.Range(TopLeftTeleportZone.y, BottomRightTeleportZone.y);
+
+        transform.position = new Vector3(TargetX, TargetY);
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    public void ThrowShuriken()
+    {
+        Vector3 direction = new Vector2(Target.transform.position.x - transform.position.x, Target.transform.position.y - transform.position.y);
+        Instantiate(Shuriken, transform.position + direction.normalized * ShurikenSpawnDistance, Quaternion.identity).GetComponent<Rigidbody2D>().velocity = direction.normalized * ShurikenSpeed;
+    }
+}
---
+++ Assets/Scripts/Shuriken.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuriken : MonoBehaviour
+{
+    public float Damage;
+    public float LifeTime;
+    private float AliveTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        AliveTime += Time.deltaTime;
+        if (AliveTime > LifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {//we hit a player, do some damage to them
+            collision.gameObject.GetComponent<Player>().TakeDamage(Damage);
+            Destroy(gameObject);  
+        }
+    }
+}
