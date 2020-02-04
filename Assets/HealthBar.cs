ca3bb12fa059c8e3dcbb825c6ec9c345ebcf03ba
Ludo <l.bitti94@gmail.com>
2020-02-04 17:23:30 +0000

    Added Player HealthBar.

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Player player;
+    private Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = player.Health / player.MaxHealth;
+    }
+}
