2d21dba405ae212ff6de1e337b783a25c167fa05
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-02-19 15:46:15 +0000

    Started working on parser for bark lines

---
+++ Assets/Scripts/BarkEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public enum BarkEventTypes {PowerupPickup, KillEnemy, NewRound, HalfHealth, NearDeath, Death, WaveCleared, NewWeapon, ThrowGun, NewSkin, NewPowerup, Achievement, Boss}
+
+public class BarkEvents : MonoBehaviour
+{
+    public string BarkLinesFile;
+    private Dictionary<BarkEventTypes, string[]> AllLines = new Dictionary<BarkEventTypes, string[]>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] lines = File.ReadAllLines(BarkLinesFile);
+        lines = (from line in lines where !line.StartsWith("#") select line).ToArray();
+        foreach (string line in lines)
+        {
+            print(line);
+        }
+    }
+
+    public static void TriggerBarkLine(BarkEventTypes type)
+    {
+
+    }
+}
