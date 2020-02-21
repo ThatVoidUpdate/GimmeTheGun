ebd754b4a9ec9a33a6d1a61ff7dde2d1f7fe7500
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2020-02-21 10:37:42 +0000

    Added configurable probability for powerups

--- Assets/Scripts/PowerupManager.cs
+++ Assets/Scripts/PowerupManager.cs
@@ -6,6 +6,8 @@ public class PowerupManager : MonoBehaviour
     public float SecondsBetweenPowerups;
     private BoxCollider2D SpawnArea;
     private float CurrentTime = 0;
+
+    private PowerupType[] chances = new PowerupType[] { PowerupType.ChangeWeapon, PowerupType.ChangeWeapon, PowerupType.DestroyAllEnemies, PowerupType.SwitchEnemies, PowerupType.HealthUp, PowerupType.DamageUp, PowerupType.SlowEnemy, PowerupType.FastPlayer, PowerupType.Bomb, PowerupType.Drunk, PowerupType.Weed, PowerupType.Cocaine, PowerupType.HotPotato, PowerupType.StickyGun, PowerupType.fourTwenty, PowerupType.Disco, PowerupType.LSD };
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,7 @@ public class PowerupManager : MonoBehaviour
         CurrentTime += Time.deltaTime;
         if (CurrentTime > SecondsBetweenPowerups)
         {//Spawn a new powerup
-            PowerupType type = (PowerupType)Random.Range(0, System.Enum.GetNames(typeof(PowerupType)).Length); //Choose a random type of powerup
+            PowerupType type = chances[Random.Range(0, chances.Length)]; //Choose a random type of powerup
             //PowerupType type = PowerupType.Weed;
             Vector3 Position = new Vector3(Random.Range(SpawnArea.offset.x - SpawnArea.size.x / 2, SpawnArea.offset.x + SpawnArea.size.x / 2), Random.Range(SpawnArea.offset.y - SpawnArea.size.y / 2, SpawnArea.offset.y + SpawnArea.size.y / 2)); //Choose a random position
             GameObject CurrentPowerup = Instantiate(PowerupPrefab, Position, Quaternion.identity); //Spawn the powerup
