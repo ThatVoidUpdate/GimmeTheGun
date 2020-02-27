19336f7d95ee1827222239996fa0ecb082c37af3
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2020-02-27 14:10:33 +0000

    Removed menu navigation system, which i wrote myself because IM SO FUCKING DUMB

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject MainObject;
+    public GameObject SettingsObject;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Complete2Player");
+    }
+
+    public void ActivateSettings()
+    {
+        MainObject.SetActive(false);
+        SettingsObject.SetActive(true);
+
+    }
+
+    public void ActivateMain()
+    {
+        MainObject.SetActive(true);
+        SettingsObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
