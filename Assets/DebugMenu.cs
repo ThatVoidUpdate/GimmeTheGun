32dfffcf6eee1ca2e40a8e415a3185d8db3db0f8
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2020-02-27 16:34:43 +0000

    Added in resolution and fullscreen options to options menu. Added debug menu showing enemy locations

---
+++ Assets/DebugMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugMenu : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            text.enabled = true;
+        }
+
+        if (text.enabled)
+        {
+            text.text = "";
+            foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+            {
+                text.text += enemy.transform.position + "\n";
+            }
+        }
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -30,4 +30,27 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void ChangeResolution(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Screen.SetResolution(800, 600, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetFullscreen(bool Fullscreen)
+    {
+        Screen.fullScreen = Fullscreen;
+    }
 }
