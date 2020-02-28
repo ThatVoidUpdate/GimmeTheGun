90c3b7ac217bc4352b1c9385d24bcc3c8985a4a4
Andrew <andrewshawley1@gmail.com>
2020-02-28 11:06:22 +0000

    Implemented audio tracks (sort of)

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public enum Backgrounds {Rocky, Lava, Arctic}
 public class Background : MonoBehaviour
 {
+    public AudioClip RockyMusic;
+    public AudioClip LavaMusic;
+    public AudioClip ArcticMusic;
+    public AudioSource source;
     public Backgrounds background;
     // Start is called before the first frame update
     void Start()
     {
+        source = GetComponent<AudioSource>();
         switch (background)
         {
             case Backgrounds.Rocky:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Rocky");
+                source.clip = RockyMusic;
                 break;
             case Backgrounds.Lava:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Lava");
+                source.clip = LavaMusic;
                 break;
             case Backgrounds.Arctic:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Arctic");
+                source.clip = ArcticMusic;
                 break;
             default:
                 break;
