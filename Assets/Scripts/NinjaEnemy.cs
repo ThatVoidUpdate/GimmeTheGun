d5bcbf54da37ddc048afebaa5d3d975f0ff30e9b
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2020-03-06 10:11:09 +0000

    fixed bug #1, ninja indicator not disappearing on enemy kill

--- Assets/Scripts/NinjaEnemy.cs
+++ Assets/Scripts/NinjaEnemy.cs
@@ -47,14 +47,21 @@ public class NinjaEnemy : Enemy
             }
             Vector3 difference = Target.transform.position - transform.position;
             AttackIndicator.SetActive(true);
+
+            //set roation of indicator
             Vector3 angles = AttackIndicator.transform.eulerAngles;
             angles.z = Vector3.SignedAngle(difference, Vector3.right, Vector3.back);
             AttackIndicator.transform.eulerAngles = angles;
+
+            //set position of indicator
             AttackIndicator.transform.position = transform.position + (difference / 2);
+
+            //set length of indicator
             Vector3 scale = AttackIndicator.transform.localScale;
             scale.x = difference.magnitude / 10;
             AttackIndicator.transform.localScale = scale;
 
+            //set transparency of indicator
             float ChargeTimeLeft = 1 - ((ThrowTime - CurrentTime) / ThrowTime);
             Color colour = AttackIndicator.GetComponent<SpriteRenderer>().color;
             colour.a = ChargeTimeLeft;
@@ -76,5 +83,10 @@ public class NinjaEnemy : Enemy
         Vector3 direction = new Vector2(Target.transform.position.x - transform.position.x, Target.transform.position.y - transform.position.y);
         Instantiate(Shuriken, transform.position + direction.normalized * ShurikenSpawnDistance, Quaternion.identity).GetComponent<Rigidbody2D>().velocity = direction.normalized * ShurikenSpeed;
         AttackIndicator.SetActive(false);
+    }
+
+    public void OnDestroy()
+    {
+        Destroy(AttackIndicator);
     }
 }
