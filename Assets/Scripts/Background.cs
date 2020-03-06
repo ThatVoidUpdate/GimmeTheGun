7eaa5539161090211826e32bdf87a0e70fcc1666
Andrew <andrewshawley1@gmail.com>
2020-03-06 11:49:48 +0000

    Added audio tracks Assigned 2 to each background Music set to loop

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public enum Backgrounds {Rocky, Lava, Arctic, Forest}
 public class Background : MonoBehaviour
 {
-    public AudioClip RockyMusic;
-    public AudioClip LavaMusic;
-    public AudioClip ArcticMusic;
-    public AudioClip ForestMusic;
+    public AudioClip[] RockyMusic;
+    public AudioClip[] LavaMusic;
+    public AudioClip[] ArcticMusic;
+    public AudioClip[] ForestMusic;
     public AudioSource source;
     public Backgrounds background;
     // Start is called before the first frame update
@@ -19,24 +19,38 @@ public class Background : MonoBehaviour
         {
             case Backgrounds.Rocky:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Rocky");
-                source.clip = RockyMusic;
+                StartCoroutine(PlayMusic(RockyMusic));
                 break;
             case Backgrounds.Lava:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Lava");
-                source.clip = LavaMusic;
+                StartCoroutine(PlayMusic(LavaMusic));
                 break;
             case Backgrounds.Arctic:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Arctic");
-                source.clip = ArcticMusic;
+                StartCoroutine(PlayMusic(ArcticMusic));
                 break;
             case Backgrounds.Forest:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Forest");
-                source.clip = ForestMusic;
+                StartCoroutine(PlayMusic(ForestMusic));
                 break;
             default:
                 break;
         }
-        source.Play();
+ 
+    }
+    public IEnumerator PlayMusic(AudioClip[] clips)
+    {
+        while (true)
+        {
+            foreach (AudioClip clip in clips)
+            {
+                source.clip = clip;
+                source.Play();
+                yield return new WaitForSeconds(source.clip.length);
+                
+            }
+        }
+        
     }
 
     // Update is called once per frame
