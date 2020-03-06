978d8cea90a202371d8eb63666cfd9f5d9c2aee3
Andrew <andrewshawley1@gmail.com>
2020-03-06 11:03:52 +0000

    Added audio tracks and updated Background script to contain Forest

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Backgrounds {Rocky, Lava, Arctic}
+public enum Backgrounds {Rocky, Lava, Arctic, Forest}
 public class Background : MonoBehaviour
 {
     public AudioClip RockyMusic;
     public AudioClip LavaMusic;
     public AudioClip ArcticMusic;
+    public AudioClip ForestMusic;
     public AudioSource source;
     public Backgrounds background;
     // Start is called before the first frame update
@@ -27,6 +28,10 @@ public class Background : MonoBehaviour
             case Backgrounds.Arctic:
                 GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Arctic");
                 source.clip = ArcticMusic;
+                break;
+            case Backgrounds.Forest:
+                GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Art/Backgrounds/Forest");
+                source.clip = ForestMusic;
                 break;
             default:
                 break;
