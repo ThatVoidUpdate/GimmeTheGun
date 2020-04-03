b9d6356b0b01601857011fd4759e28eab635e156
HyperLives <58257241+HyperLives@users.noreply.github.com>
2020-04-03 11:18:25 +0100

    Added Audio Slider in UI and redo mixer

---
+++ Assets/Scripts/MasterMixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MasterMixer : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+
+    public AudioMixer musicmixer;
+    // Start is called before the first frame update
+   
+
+        public void SetVolume (float volume)
+        {
+            audiomixer.SetFloat ("auidomixer", volume);
+            //volume = pubaud;
+            PlayerPrefs.SetFloat ("GlobalVol", volume);
+            PlayerPrefs.Save();
+        }
+
+        public void SetVolumeMusic (float volume)
+        {
+            musicmixer.SetFloat ("musicmixer", volume);
+        }
+
+    // Update is called once per frame
+}
