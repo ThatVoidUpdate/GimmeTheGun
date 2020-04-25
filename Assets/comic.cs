b2ce76f5c7326943104a62f4670148702375c01e
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-04-25 10:29:50 +0100

    Finished comic strip, just need to add in correct comic and timings

--- Assets/comic.cs
+++ Assets/comic.cs
@@ -1,18 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class comic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Sprite[] panels;
+    public float[] timings;
+
+    public float FadeTime;
+
+    public Image displayImage;
+    private int index = 0;
+
     void Start()
     {
-        
+        if (panels.Length != timings.Length)
+        {
+            Debug.LogError("There needs to be a time for every comic (comic.cs)");
+        }
+        displayImage.sprite = panels[index];
+        StartCoroutine(ComicWait());
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //wait for timings[index]
+
+        //fade out
+        //change panel
+        //fade in
+        //repeat
+    }
+
+    public IEnumerator ComicWait()
+    {
+        yield return new WaitForSeconds(timings[index]);
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeOut()
+    {
+        print("Fading out");
+        float currentFade = FadeTime;
+
+        while (currentFade >= 0)
+        {
+            currentFade -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+            displayImage.color = new Color(currentFade / FadeTime, currentFade / FadeTime, currentFade / FadeTime);
+        }
+
+        index++;
+        if (index == timings.Length)
+        {
+            //reached end of comic
+            SceneManager.LoadScene("Complete2Player");
+        }
+        else
+        {
+            displayImage.sprite = panels[index];
+            StartCoroutine(FadeIn());
+        }
+
+    }
+
+    public IEnumerator FadeIn()
+    {
+        print("Fading in");
+        float currentFade = 0;
+
+        while (currentFade <= FadeTime)
+        {
+            currentFade += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+            displayImage.color = new Color(currentFade / FadeTime, currentFade / FadeTime, currentFade / FadeTime);
+        }
+
+        StartCoroutine(ComicWait());
     }
 }
