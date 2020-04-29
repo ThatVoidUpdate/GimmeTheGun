71799b7a1d1855769d19ebe76180384430b10da8
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-04-29 15:03:14 +0100

    Added playergraphics scriptable object to tidy up player selection

---
+++ Assets/Players/PlayerGraphics.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerGraphics", menuName = "Player Graphics")]
+public class PlayerGraphics : ScriptableObject
+{    
+    public Sprite FrontSprite;
+    public Sprite BackSprite;
+    public Sprite LeftSprite;
+    public Sprite RightSprite;
+}
